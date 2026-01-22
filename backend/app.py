a3aecc559dbc7f70792ff49b572c8e1c5015b70a
Manan Shukla <manan.shukla0708@gmail.com>
2026-01-22 18:41:38 +0530

    Add PDF uplaod API with validation and file storage

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import os
+import shutil
 
 app = FastAPI(title="PDF Screening API")
+
+UPLOAD_DIR = "data/uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/")
 def health_check():
     return {"status": "API running successfully"}
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="Only PDF files are allowed.")
+    
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    return {"filename": file.filename, "message": "File uploaded successfully"}
